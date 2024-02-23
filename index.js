895574b5bfcbf962056390f930a954ba81705fa6
hannahvw <103385894+hannahvw@users.noreply.github.com>
2024-02-23 13:16:24 -0600

    added javascript

---
+++ index.js
@@ -0,0 +1,19 @@
+const shareBtn = document.querySelector(".share-button");
+const shareBtnLight = document.querySelector(".share-button-light");
+const footer = document.querySelector(".container-footer");
+const socialMedia = document.querySelector(".container-footer--social-media");
+let buttonState = false;
+
+function showIcons() {
+  buttonState = !buttonState;
+  if (buttonState) {
+    socialMedia.style.display = "flex";
+    footer.style.display = "none";
+  } else {
+    footer.style.display = "flex";
+    socialMedia.style.display = "none";
+  }
+}
+
+shareBtn.addEventListener("click", showIcons);
+shareBtnLight.addEventListener("click", showIcons);
